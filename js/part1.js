5aaa7c9901e1aedcd51212c5aa34b109767d8f25
John Cahill <jcahill1@hwemail.com>
2020-01-16 12:04:48 -0800

    Can convert anything to base 10

--- js/part1.js
+++ js/part1.js
@@ -1,12 +1,23 @@
 function ConversionPart1() {
 
-    var UnsignedInt = parseInt(document.getElementById("1_UnsignedInt").value);
+    var UnsignedInt = document.getElementById("1_UnsignedInt").value;
     var UnsignedIntBaseFrom = parseInt(document.getElementById("1_UnsignedIntBaseToConvertFrom").value);
     var UnsignedIntBaseTo = parseInt(document.getElementById("1_UnsignedIntBaseToConvertTo").value);
 
+    //going to base ten
+    var value = 0;
+    var counter = 0;
+    while (counter<=UnsignedInt.length-1)
+    {
+      var digit = parseInt(UnsignedInt.substring(counter, counter+1));
+      value = value*UnsignedIntBaseFrom+digit;
+      counter++;
+    }
 
-
-  var outputValue = "11111111";
+    //base ten to designated base
+    var newValue =value;
+    
+  var outputValue = newValue;
 
   // Show the output on the screen
   FormatAndShowOutput([UnsignedInt, UnsignedIntBaseFrom, UnsignedIntBaseTo, outputValue], 1);
